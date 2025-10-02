15594aeeffb45a32956600f8e29ea03ec68f4749
bakhtiarmahmoodi73 <bakhtiarmahmoodi1995@gmail.com>
2025-10-02 11:39:47 -0700

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'gh-app-9yq3.vercel.app',
+            pathname: '/**',
+          },
+    ]
+    }
+};
 
 export default nextConfig;
